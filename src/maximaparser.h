8388ffd78bbe208de393506517f95e37162357ac
Jason Ansel <jansel@csail.mit.edu>
2008-12-12 14:34:54 -0500

    - added ruleir files - added output of flex/bison to fix fresh build

---
+++ src/maximaparser.h
@@ -0,0 +1,107 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     IPROMPT = 258,
+     OPROMPT = 259,
+     LE = 260,
+     GE = 261,
+     BOOL_T = 262,
+     BOOL_F = 263,
+     STR_EQUAL = 264,
+     STR_FLOOR = 265,
+     STR_CEILING = 266,
+     IDENT = 267,
+     INTEGER = 268,
+     FLOAT = 269
+   };
+#endif
+/* Tokens.  */
+#define IPROMPT 258
+#define OPROMPT 259
+#define LE 260
+#define GE 261
+#define BOOL_T 262
+#define BOOL_F 263
+#define STR_EQUAL 264
+#define STR_FLOOR 265
+#define STR_CEILING 266
+#define IDENT 267
+#define INTEGER 268
+#define FLOAT 269
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+#line 21 "maximaparser.ypp"
+{
+  bool b;
+  int i;
+  double d;
+  const char* str;
+  hecura::Formula*        formula;
+  hecura::FormulaList*    formulas;
+}
+/* Line 1489 of yacc.c.  */
+#line 86 "maximaparser.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE maximalval;
+
+#if ! defined YYLTYPE && ! defined YYLTYPE_IS_DECLARED
+typedef struct YYLTYPE
+{
+  int first_line;
+  int first_column;
+  int last_line;
+  int last_column;
+} YYLTYPE;
+# define yyltype YYLTYPE /* obsolescent; will be withdrawn */
+# define YYLTYPE_IS_DECLARED 1
+# define YYLTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYLTYPE maximalloc;
---
+++ src/ruleir.h
@@ -0,0 +1,86 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Jason Ansel                                     *
+ *   jansel@csail.mit.edu                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef HECURARULEIR_H
+#define HECURARULEIR_H
+
+#include "jrefcounted.h"
+#include "jprintable.h"
+
+namespace hecura {
+
+class RIRNode;
+class RIRBlock;
+class RIRStmt;
+class RIRExpr;
+typedef jalib::JRef<RIRNode>  RIRNodePtr;
+typedef jalib::JRef<RIRBlock> RIRBlockPtr;
+typedef jalib::JRef<RIRStmt>  RIRStmtPtr;
+typedef jalib::JRef<RIRExpr>  RIRExprPtr;
+typedef std::vector<RIRNodePtr>  RIRNodeList;
+typedef std::vector<RIRBlockPtr> RIRBlockList;
+typedef std::vector<RIRStmtPtr>  RIRStmtList;
+typedef std::vector<RIRExprPtr>  RIRExprList;
+
+/**
+ * Base class for all Rule IR types
+ */
+class RIRNode : public jalib::JRefCounted, public jalib::JPrintable {
+public:
+  void print(std::ostream& o) const { o << "RIRNode"; }
+};
+
+/**
+ * Rule IR Expression
+ */
+class RIRExpr  : public RIRNode {
+public:
+  RIRExpr(const std::string& str) :_str(str) {}
+  void addSubExpr(const RIRExprPtr& p) { _parts.push_back(p); }
+  void print(std::ostream& o) const { o << _str; }
+private:
+  std::string _str;
+  RIRExprList _parts;
+};
+
+/**
+ * Rule IR Statement
+ */
+class RIRStmt  : public RIRNode {
+public:
+  void addExpr(const RIRExprPtr& p) { _exprs.push_back(p); }
+private:
+  RIRExprList _exprs;
+};
+
+/**
+ * Rule IR Basic Block
+ */
+class RIRBlock : public RIRNode {
+public:
+  
+  void addStmt(const RIRStmtPtr& p) { _stmts.push_back(p); }
+private:
+  RIRStmtList _stmts;
+};
+
+
+}
+
+#endif
