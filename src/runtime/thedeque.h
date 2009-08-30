477c097f9b9c0981591d371b81cda867ab1c1c97
Jason Ansel <jansel@csail.mit.edu>
2009-08-30 18:44:17 -0400

    - switch to THE based semi-lock-free deque

--- src/runtime/thedeque.h
+++ src/runtime/thedeque.h
@@ -31,9 +31,114 @@
 
 namespace petabricks {
 
+/** 
+ * Implementation of the Cilk THE protocol for a work stealing deque 
+ */
 template < typename T >
 class THEDeque {
 #if 1
+public:
+  THEDeque() {
+    _t = 0;
+    _h = 0;
+    _size = 1000;
+    _array = (T*) malloc(sizeof(T) * _size); // Use malloc so we can realloc
+  }
+  ~THEDeque() {
+    free(_array);
+  }
+
+  ///
+  /// Called by owner thread
+  void push_top(const T& task) {
+    if(empty() && _h>0){
+      //reset the head/tail pointers
+      JLOCKSCOPE(_lock);
+      if(empty())
+        _t=_h=0;
+    }
+
+    _array[_t] = task;
+    _t++;
+
+    if (_t >= _size - 1) {
+      //resize the array
+      JLOCKSCOPE(_lock);
+      _size *= 3;
+      _array = (T*) realloc(_array, sizeof(T) * _size);
+    }
+  }
+
+  ///
+  /// Called by owner thread
+  T pop_top() {
+
+    if (empty()) {
+      return NULL;
+    }
+
+    _t--;
+
+    jalib::memFence();
+
+    if (_h > _t) {
+      _t++;
+      {
+        JLOCKSCOPE(_lock);
+        if (empty()) {
+          return NULL;
+        }
+        _t--;
+        return _array[_t];
+      }
+    } else {
+      return _array[_t];
+    }
+  }
+
+  ///
+  /// Called by stealer thread
+  T pop_bottom() {
+
+    if (empty() || !_lock.trylock()) {
+      return NULL;
+    }
+
+    _h++;
+
+    jalib::memFence();
+
+    if (_h > _t) {
+      _h--;
+      _lock.unlock();
+      return NULL;
+    }
+
+    T task = _array[_h - 1];
+
+    _lock.unlock();
+
+    return task;
+  }
+
+  bool empty() const {
+    return _h >= _t;
+  }
+  int size() const {
+    return (int)(_t - _h);
+  }
+private:
+  long _size;
+  T* _array;
+  PADDING(CACHE_LINE_SIZE);
+  long _h; // head index
+  PADDING(CACHE_LINE_SIZE);
+  long _t; // tail index
+  PADDING(CACHE_LINE_SIZE);
+  jalib::JMutex _lock;
+  PADDING(CACHE_LINE_SIZE);
+#else
+//THIS IS IS SIMPLIFIED/SLOW VERSION FOR TESTING:
 public:
   void push_top(const T& task) { 
     JLOCKSCOPE(_lock);
@@ -61,148 +166,6 @@ public:
 private:
   std::deque<T> _deque;
   jalib::JMutex _lock;
-
-#endif
-#if 0
-  private:
-    long _size;
-    T* _array;
-    PADDING(CACHE_LINE_SIZE);
-    long _h; // head index
-    PADDING(CACHE_LINE_SIZE);
-    long _t; // tail index
-    PADDING(CACHE_LINE_SIZE);
-    jalib::JMutex _lock;
-    PADDING(CACHE_LINE_SIZE);
-  public:
-
-  THEDeque() {
-    _t = 0;
-    _h = 0;
-    _size = 1000;
-    _array = (T*) malloc(sizeof(T) * _size); // Use malloc so we can realloc
-  }
-
-  ~THEDeque() {
-    free(_array);
-  }
-
-  void push_top(const T& task) {
-
-    _array[_t] = task;
-    _t++;
-
-    if (_t >= _size - 1) {
-      JLOCKSCOPE(_lock);
-
-      _size *= 3;
-      _array = (T*) realloc(_array, sizeof(T) * _size);
-    }
-  }
-
-#ifdef THEDEQUE_DISABLE_LOCKFREE
-
-  T pop_top() {
-
-    if (isEmpty()) {
-      return NULL;
-    }
-
-    JLOCKSCOPE(_lock);
-
-    if (_h == _t) {
-      return NULL;
-    } else {
-      _t--;
-      return _array[_t];
-    }
-  }
-
-  T pop_bottom() {
-
-    if (isEmpty() || !_lock.trylock()) {
-      return NULL;
-    }
-
-    T retVal = NULL;
-
-    if (_h != _t) {
-      retVal = _array[_h];
-      _h++;
-    }
-
-    _lock.unlock();
-
-    return retVal;
-  }
-
-#else
-
-  T pop_top() {
-
-    if (isEmpty()) {
-      return NULL;
-    }
-
-    _t--;
-
-    jalib::memFence();
-
-    if (_h > _t) {
-      _t++;
-      {
-        JLOCKSCOPE(_lock);
-        if (_h >= _t) {
-          return NULL;
-        }
-        _t--;
-        return _array[_t];
-      }
-    } else {
-      return _array[_t];
-    }
-  }
-
-  T pop_bottom() {
-
-    if (isEmpty() || !_lock.trylock()) {
-      return NULL;
-    }
-
-    _h++;
-
-    jalib::memFence();
-
-    if (_h > _t) {
-      _h--;
-      _lock.unlock();
-      return NULL;
-    }
-
-    T task = _array[_h - 1];
-
-    _lock.unlock();
-
-    return task;
-  }
-
-#endif
-
-  void clear() {
-    JLOCKSCOPE(_lock);
-
-    for (int i = _h; i < _t; i++) {
-      _array[i]->runWrapper(true);
-    }
-
-    _h = 0;
-    _t = 0;
-  }
-
-  bool isEmpty() const {
-    return _h == _t;
-  }
-
 #endif
 };
 }
