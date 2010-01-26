3d966e4f704d0a1ddd3885d8860e390c68ab1339
Jason Ansel <jansel@csail.mit.edu>
2010-01-26 00:10:40 -0500

    .

--- scripts/candidatetester.py
+++ scripts/candidatetester.py
@@ -19,11 +19,13 @@ def tmpcfgfile(n=0):
 class Results:
   '''stores a list of (timing|accuracy) test results and collects statistics'''
   def __init__(self, results=[]):
-    self.results=results   #listof(float)
-    self.timeoutResults=[] #listof(float)
+    self.realResults=[]         #listof(float)
+    self.timeoutResults=[]      #listof(float)
+    self.interpolatedResults=[] #listof(float)
+    self.distribution = None
   def __repr__(self):
     v=[]
-    v.extend(map(lambda x: "%.4f"%x, self.results))
+    v.extend(map(lambda x: "%.4f"%x, self.realResults))
     v.extend(map(lambda x: ">%.4f"%x, self.timeoutResults))
     return ', '.join(v)
   def __str__(self):
@@ -35,39 +37,49 @@ class Results:
     delta = b-a
     return "%.4f (+- %.4f)" % (b, delta)
   def __len__(self):
-    return len(self.results)+len(self.timeoutResults)
-  def dataDistribution(self):
+    return len(self.interpolatedResults)
+  def reinterpolate(self):
     '''estimated probability distribution of a single timing run'''
-    assert len(self)>0
-    mkdistrib = lambda points: stats.norm(*stats.norm.fit(points))
-    points = list(self.results)
-    if len(points) == 0:
+    self.interpolatedResults = list(self.realResults)
+    mkdistrib = lambda: stats.norm(*stats.norm.fit(self.interpolatedResults))
+    if len(self.interpolatedResults) == 0:
       '''all tests timed out, seed with double the average timeout'''
-      points.append(sum(self.timeoutResults)/len(self.timeoutResults)*2.0)
-    if len(points) == 1:
+      self.interpolatedResults.append(sum(self.timeoutResults)/len(self.timeoutResults)*2.0)
+    if len(self.interpolatedResults) == 1:
       '''only 1 test, use prior stddev'''
-      dd = stats.norm(points[0], points[0]*config.prior_stddev_pct)
+      self.distribution = stats.norm(self.interpolatedResults[0], self.interpolatedResults[0]*config.prior_stddev_pct)
     else:
       '''estimate stddev with least squares'''
-      dd = mkdistrib(points)
+      self.distribution = mkdistrib()
     for p in sorted(self.timeoutResults):
       '''now lets estimate values for the points that timed out'''
       '''new points are assigned the median value above their timeout'''
-      points.append(max(p, min(dd.isf(dd.sf(p)/2.0), p*2)))
-      dd = mkdistrib(points)
-    return dd
+      points.append(max(p, min(self.distribution.isf(dd.sf(p)/2.0), p*2)))
+      self.distribution = mkdistrib()
+  def dataDistribution(self):
+    return self.distribution
   def meanDistribution(self):
     '''estimated probability distribution of the real mean value'''
     dd=self.dataDistribution()
     mean, var = dd.stats()
     var/=len(self)
     return stats.norm(mean, math.sqrt(var))
+  def add(self, p):
+    self.realResults.append(p)
+    self.reinterpolate();
+  def addTimeout(self, p):
+    self.timeoutResults.append(p)
+    self.reinterpolate();
+  def ttest(self, that):
+    assert len(self)>0
+    assert len(that)>0
+    return stats.ttest_ind(self.interpolatedResults, that.interpolatedResults, 0)[1]
 
 class ResultsDB:
   '''stores many Results for different input sizes'''
   def __init__(self, metric, vals=dict()):
     self.metric = metric
-    self.nToResults = vals
+    self.nToResults = dict(vals)
   def __getitem__(self, n):
     try:
       return self.nToResults[n]
@@ -85,7 +97,7 @@ class Candidate:
   '''A candidate algorithm in the population'''
   def __init__(self, cfg):
     self.config  = ConfigFile(cfg)
-    self.metrics = map(ResultsDB, config.metrics)
+    self.metrics = [ResultsDB(x) for x in config.metrics]
 
   def clone(self):
     '''this creates result *ALIASES*, not copies, so new results will be added to both algs'''
@@ -129,11 +141,11 @@ class CandidateTester:
       results = pbutil.executeRun(cmd, config.metrics)
       for i,result in enumerate(results):
         if result is not None:
-          candidate.metrics[i][self.n].results.append(result['average'])
+          candidate.metrics[i][self.n].add(result['average'])
       return True
     except pbutil.TimingRunTimeout:
       timingIdx = filter(lambda x: x[1]=='timing', enumerate(config.metrics))[0][0]
-      candidate.metrics[timingIdx][self.n].timeoutResults.append(self.timeout)
+      candidate.metrics[timingIdx][self.n].addTimeout(self.timeout)
       return False
   def cleanup(self):
     os.unlink(self.cfgTmp)
@@ -146,13 +158,18 @@ if __name__ == "__main__":
   tester = CandidateTester(benchmark, 768, 5.0)
   try:
     candidate = Candidate(defaultConfigFile(pbutil.benchmarkToBin(tester.app)))
+    candidate2 = Candidate(defaultConfigFile(pbutil.benchmarkToBin(tester.app)))
     tester.test(candidate)
     tester.test(candidate)
-    print len(candidate.metrics[0][768])
+    tester.test(candidate)
+    tester.test(candidate2)
+    tester.test(candidate2)
+    tester.test(candidate2)
     print candidate.metrics[0]
+    print candidate2.metrics[0]
     print str(candidate.metrics[0][768])
-    print candidate.metrics[0][768].dataDistribution().stats()
-    print candidate.metrics[0][768].meanDistribution().stats()
+    print str(candidate2.metrics[0][768])
+    print candidate.metrics[0][768].ttest(candidate2.metrics[0][768])
   finally:
     tester.cleanup()
 
