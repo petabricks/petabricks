d4ec16ab6f96408bdc03c0a17b21a50d7a527346
Jason Ansel <jansel@csail.mit.edu>
2011-05-03 13:23:21 -0400

    - csvavg update, gitignore, pbbenchmark

--- scripts/misc/csvavg.py
+++ scripts/misc/csvavg.py
@@ -3,10 +3,13 @@
 import csv, sys
 import numpy
 
-inputs = map(csv.DictReader, map(open, sys.argv[1:]))
+dialect = csv.excel_tab
+multi_file=len(sys.argv[1:])>1
+
+inputs = map(lambda x: csv.DictReader(x, dialect=dialect), map(open, sys.argv[1:]))
 rows = map(csv.DictReader.next, inputs)
 headers = inputs[0].fieldnames
-output = csv.writer(sys.stdout)
+output = csv.writer(sys.stdout, dialect=dialect)
 output.writerow(headers)
 
 def mkavg(k):
@@ -16,11 +19,31 @@ def mkavg(k):
   except:
     return 'error'
 
-try:
-  while True:
-    output.writerow(map(mkavg, headers))
-    rows = map(csv.DictReader.next, inputs)
-except StopIteration:
-  pass
+if multi_file:
+  try:
+    while True:
+      output.writerow(map(mkavg, headers))
+      rows = map(csv.DictReader.next, inputs)
+  except StopIteration:
+    pass
+else:
+  counts=dict()
+  sums=dict()
+  for k in headers:
+    try:
+      sums[k]=float(rows[0][k])
+    except:
+      sums[k]=0.0
+    counts[k]=1.0
+  for row in inputs[0]:
+    for k in headers:
+      try:
+        sums[k]+=float(row[k])
+      except:
+        sums[k]=0.0
+      counts[k]+=1.0
+
+
+  output.writerow(map(lambda k: sums[k]/counts[k], headers))
 
 
