d5fb600d9fddc637388b6b3ac2a3821509e47f28
Cy Chan (yoshi-xubuntu) <cychan@csail.mit.edu>
2011-08-11 16:12:04 -0400

    nwkde updates:   pulled generators and metric transforms into separate files   created header file

---
+++ examples/kernel/nwkde.h
@@ -0,0 +1,9 @@
+#ifndef NWKDE_H
+#define NWKDE_H
+
+//#define DEBUG
+#define WORKAROUND // workaround for generator having specific sizes
+//#define TESTDIR "/afs/csail.mit.edu/u/c/cychan/projects/petabricks/examples/kernel/test/"
+#define TESTDIR "/home/cychan/projects/petabricks/examples/kernel/test/"
+
+#endif // NWKDE_H
