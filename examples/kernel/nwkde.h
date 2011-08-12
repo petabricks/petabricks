53bc2492b2c7f4bc93eb5be9a40f5d0e7c7cb0ae
Cy Chan (yoshi-xubuntu) <cychan@csail.mit.edu>
2011-08-11 21:00:03 -0400

    changed size parameters of main transform inputs preprocessor defines in an   attempt to simplify control.  exposed some bugs in preprocessor:     BUG: some transform analysis appears to be done before preprocessor subs.       constant variable dimensions are changed by --n during autotuning.       non-integer size of tunable array causes parse error.     BUG: some issues with #ifdef in nwkde.h.  had to comment out body of       #else clause to prevent mysterious unwanted substitutions.

--- examples/kernel/nwkde.h
+++ examples/kernel/nwkde.h
@@ -2,8 +2,38 @@
 #define NWKDE_H
 
 //#define DEBUG
-#define WORKAROUND // workaround for generator having specific sizes
+
+// workaround for generator having specific sizes
+#define WORKAROUND
+
 //#define TESTDIR "/afs/csail.mit.edu/u/c/cychan/projects/petabricks/examples/kernel/test/"
 #define TESTDIR "/home/cychan/projects/petabricks/examples/kernel/test/"
+
+// Problem dimensions
+#ifdef WORKAROUND
+
+// make sure generator and metric transform headers are compatible
+#define M 4
+#define N 8750
+#define L l
+#define P 8
+#define Q q
+#define M2 4
+#define N2 8750
+
+#else
+
+// PPBUG: uncommenting this section causes M to be replaced incorrectly
+//   in nwkdeVA.pbcc rule body
+
+//#define M m
+//#define N n
+//#define L l
+//#define P p
+//#define Q q
+//#define M2 m2
+//#define N2 n2
+
+#endif
 
 #endif // NWKDE_H
