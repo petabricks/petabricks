297177bce9e582f490f283b4303f00f4e83e5da5
Cy Chan (yoshi-xubuntu) <cychan@csail.mit.edu>
2011-08-13 15:00:18 -0400

    simplified code   PP bugs fixed

--- examples/kernel/nwkde.h
+++ examples/kernel/nwkde.h
@@ -6,13 +6,13 @@
 // workaround for generator having specific sizes
 #define WORKAROUND
 
-//#define TESTDIR "/afs/csail.mit.edu/u/c/cychan/projects/petabricks/examples/kernel/test/"
-#define TESTDIR "/home/cychan/projects/petabricks/examples/kernel/test/"
+// relative directory for test data used by generator
+#define TESTDIR "examples/kernel/test/"
 
 // Problem dimensions
 #ifdef WORKAROUND
 
-// make sure generator and metric transform headers are compatible
+// make sure generator headers are compatible
 #define M 4
 #define N 8750
 #define L l
@@ -23,16 +23,13 @@
 
 #else
 
-// PPBUG: uncommenting this section causes M to be replaced incorrectly
-//   in nwkdeVA.pbcc rule body
-
-//#define M m
-//#define N n
-//#define L l
-//#define P p
-//#define Q q
-//#define M2 m2
-//#define N2 n2
+#define M m
+#define N n
+#define L l
+#define P p
+#define Q q
+#define M2 m2
+#define N2 n2
 
 #endif
 
